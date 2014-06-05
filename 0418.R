bc871195dacbe3e90ab0cae3edc621f7509c3311
Noel Jiwanmall <noeljiwanmall@yahoo.com>
2014-06-04 17:39:43 -0700

    add 0418.R

---
+++ 0418.R
@@ -0,0 +1,24 @@
+winners <- read.delim('clipboard')
+winners
+Romeo <- 18
+Juliet<-42
+Oscar <-44
+Sierra <- 17
+Oscar
+Juliet - Romeo
+(Romeo+Juliet+Oscar+Sierra)/4
+Average <- (Romeo+Juliet+Oscar+Sierra)/4
+Average
+c('Romeo','Juliet','Oscar','Sierra')
+names<-c('Romeo','Juliet','Oscar','Sierra')
+names
+c(1,2,7, .25)
+1:10
+list<-1:10
+list
+seq(from=2,to=20,by=2)
+seq(from=1,to=7)
+rep(names,3)
+rep(names,each=3)
+rep(1:4,3)
+rep(list, 2)
